0a63c92e3ee1f3bcd69b067f94a31b1753b83ed7
FireCodingDay <69696645+parthuarnax@users.noreply.github.com>
2021-06-24 22:46:07 +0900

    10%

---
+++ Blossom/app/src/main/java/com/example/blossom/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.blossom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        btn_map = findViewById(R.id.btn_map);
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , Map.class);
+                startActivity(intent); // 액티비티 이동
+            }
+        });
+
+        btn_map = findViewById(R.id.btn_memo);
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , Memo.class);
+                startActivity(intent); // 액티비티 이동
+            }
+        });
+
+
+    }
+}
